a98ac0952846c85c7f5c869f5bb1dedad6881329
Jörg Hofmann <joerg@thuenic.de>
2021-07-05 20:38:13 +0200

    pihole-Docker fertig

---
+++ scripts/pihole-provision.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# APT im nicht-interaktiven Modus
+export DEBIAN_FRONTEND=noninteractive
+
+# Docker installieren
+apt-get -y install apt-transport-https ca-certificates curl \
+    gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+    | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io \
+  git docker-compose
+systemctl enable --now docker
+adduser vagrant docker
+
+# Konfiguration der Namensauflösung
+systemctl disable --now systemd-resolved
+rm /etc/resolv.conf
+touch /etc/resolv.conf
+
+# pihole - Container
+cd /vagrant && docker-compose up -d
+
